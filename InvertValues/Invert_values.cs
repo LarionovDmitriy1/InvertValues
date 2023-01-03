e9fa6f9176ccc0fb681ea7ab7330a566c474de41
Дима Ларионов <dima_larionov_000@mail.ru>
2023-01-03 13:58:13 +0300

    Добавьте файлы проекта.

---
+++ InvertValues/Invert_values.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvertValues;
+
+public class Invert_values
+{
+    public static int[] InvertValues(int[] input)
+    {
+        string temp = "";
+        List<int> list = new List<int>();
+        foreach (int i in input)
+        {
+            if (i == 0 && input.Length == 1) { return new int[] { 0 }; }
+            if (i < 0)
+            {
+                temp = i.ToString();
+                temp = temp.Replace("-", "");
+                int a = int.Parse(temp);
+                list.Add(a);
+            }
+            else if (i > 0)
+            {
+                temp = i.ToString();
+                temp = '-' + temp;
+                int a = int.Parse(temp);
+                list.Add(a);
+            }
+            else if (i == 0)
+            {
+                list.Add(i);
+            }
+        }
+        input = list.ToArray();
+        return input;
+    }
+}
---
+++ InvertValues/Program.cs
@@ -0,0 +1,7 @@
+﻿using InvertValues;
+
+var result = Invert_values.InvertValues(new int[] { 45, 2, 21, 2, 13 - 432, -32, -34, 0, -0 });
+foreach (var value in result)
+{
+    Console.Write($"{value} ");
+}
